80436e2efd15ae19d70be2ba24de0c22bcf0d370
Bartłomiej Redak <s25197@szuflandia.pjwstk.edu.pl>
2021-11-06 14:13:44 +0100

    Pierwszy kod kalkulatora

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(){
+
+int a, b, c;
+float srednia;
+std::cout<<"podaj liczbe a: ";
+std::cin>>a;
+std::cout<<"podaj liczbe b: ";
+std::cin>>b;
+std::cout<<"podaj liczbe c: ";
+std::cin>>c;
+
+srednia=(a+b+c)/3;
+
+std::cout<<"Twoja srednia to: "<<std::endl;
+
+return 0;
+}
